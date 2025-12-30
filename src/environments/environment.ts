5b61a9cb5969efd91aad368a311ed769705ec94b
Vishal Singh <111641401+Vishal368-singh@users.noreply.github.com>
2025-12-30 12:05:53 +0530

    Comment out Org Server apiUrl in environment.ts
    Comment out the existing apiUrl for the Org Server.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://192.168.1.21:6633',
-  apiUrl: 'https://mlinfomap.org/weatherapi', // Org Server
+  // apiUrl: 'https://mlinfomap.org/weatherapi', // Org Server
   // apiUrl: 'https://weather.mlinfomap.com/weatherapi', // Server
 };
